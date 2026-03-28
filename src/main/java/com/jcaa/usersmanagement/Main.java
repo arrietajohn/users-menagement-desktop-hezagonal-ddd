011907df427ba0817cd89d495a6f03a8f3cae4a0
John Carlos Arrieta Arrieta <johncarlosarrietaarrieta@gmail.com>
2026-03-28 18:52:56 -0500

    chore/configuracion basica del proyecto

---
+++ src/main/java/com/jcaa/usersmanagement/Main.java
@@ -0,0 +1,9 @@
+package com.jcaa.usersmanagement;
+
+
+public class Main {
+
+  public static void main(String[] args) {
+      System.out.println("Inicio del proyecto. By John Carlos  Arrieta Arrieta");
+  }
+}
