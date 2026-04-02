ab6d9f69d3d2ea764a2be0d14d3f81e5a6a2ef5c
John Carlos Arrieta Arrieta <johncarlosarrietaarrieta@gmail.com>
2026-04-01 21:23:56 -0500

    feat: integrate SLF4J logging in the main application

--- src/main/java/com/jcaa/usersmanagement/Main.java
+++ src/main/java/com/jcaa/usersmanagement/Main.java
@@ -1,9 +1,12 @@
 package com.jcaa.usersmanagement;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+  private static final Logger log = LoggerFactory.getLogger(Main.class);
 
   public static void main(String[] args) {
-      System.out.println("Inicio del proyecto. By John Carlos  Arrieta Arrieta");
+    log.info("Inicio del proyecto. By John Carlos Arrieta Arrieta");
   }
 }
