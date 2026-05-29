46b649a031788e19e8f3fb958d52130f0d8200bf
Isaac Vasquez <ivasquezr@unicartagena.edu.co>
2026-05-29 12:00:10 -0500

    test: saving prgress before merge

--- src/main/java/com/jcaa/usersmanagement/Main.java
+++ src/main/java/com/jcaa/usersmanagement/Main.java
@@ -25,8 +25,9 @@ public class Main {
       System.out.println("          SISTEMA PRINCIPAL            ");
       System.out.println("=======================================");
       System.out.println("1. Gestionar Usuarios");
-      System.out.println("2. Gestionar Habitaciones");
-      System.out.println("3. Salir del Sistema");
+      System.out.println("2. Gestionar Actividades");
+      System.out.println("3. Gestionar Habitaciones");
+      System.out.println("4. Salir del Sistema");
       System.out.print("Seleccione un módulo: ");
 
       String option = scanner.nextLine();
@@ -38,12 +39,17 @@ public class Main {
           UserManagementCli userCli = new UserManagementCli(container.userController(), console);
           userCli.start();
         }
+
         case "2" -> {
+
+        }
+
+        case "3" -> {
 
           RoomManagementCli roomCli = new RoomManagementCli(container.roomController());
           roomCli.start();
         }
-        case "3" -> {
+        case "4" -> {
           System.out.println("Cerrando el sistema. Adiós");
           running = false;
         }
