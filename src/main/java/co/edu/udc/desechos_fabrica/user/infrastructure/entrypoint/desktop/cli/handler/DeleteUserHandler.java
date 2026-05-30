239d43e28a6ca8b41b3ee71dc004a20c4ce36e35
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-05-30 16:03:26 -0500

    feat(infrastructure): add permission validation for user deletion

--- src/main/java/co/edu/udc/desechos_fabrica/user/infrastructure/entrypoint/desktop/cli/handler/DeleteUserHandler.java
+++ src/main/java/co/edu/udc/desechos_fabrica/user/infrastructure/entrypoint/desktop/cli/handler/DeleteUserHandler.java
@@ -1,8 +1,10 @@
 package co.edu.udc.desechos_fabrica.user.infrastructure.entrypoint.desktop.cli.handler;
 
+import co.edu.udc.desechos_fabrica.user.domain.enums.UserRole;
 import co.edu.udc.desechos_fabrica.user.domain.exception.UserNotFoundException;
 import co.edu.udc.desechos_fabrica.user.infrastructure.entrypoint.desktop.cli.io.ConsoleIO;
 import co.edu.udc.desechos_fabrica.user.infrastructure.entrypoint.desktop.controller.UserController;
+import co.edu.udc.desechos_fabrica.user.infrastructure.entrypoint.desktop.dto.UserResponse;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -13,12 +15,34 @@ public final class DeleteUserHandler implements OperationHandler {
 
   @Override
   public void handle() {
-    final String id = console.readRequired("User ID to delete: ");
+    final String actorEmail = console.readRequired("Enter your email (as administrator) to proceed: ");
+    final String email = console.readRequired("User Email to delete: ");
+
+    final UserResponse actor = userController.findUserByEmail(actorEmail);
+    final UserResponse emailToDelete = userController.findUserByEmail(email);
+
+    final UserRole actorRole = UserRole.fromString(actor.role());
+    final UserRole emailToDeleteRole = UserRole.fromString(emailToDelete.role());
+
+    if (deletePermissions(actorRole, emailToDeleteRole)) {
+      console.println("\n  Error: You do not have permission to delete this user.");
+      return;
+    }
     try {
-      userController.deleteUser(id);
+      userController.deleteUser(email);
       console.println("  User deleted successfully.");
     } catch (final UserNotFoundException exception) {
       console.println("  Not found: " + exception.getMessage());
     }
+  }
+
+  public boolean deletePermissions(UserRole actorRole, UserRole emailToDeleteRole) {
+    return switch (actorRole) {
+      case ADMIN -> false;
+      case REVIEWER -> (emailToDeleteRole == UserRole.ADMIN || emailToDeleteRole == UserRole.REVIEWER);
+      case ENTERPRISE_ADMIN ->
+              (emailToDeleteRole == UserRole.ADMIN || emailToDeleteRole == UserRole.REVIEWER || emailToDeleteRole == UserRole.ENTERPRISE_ADMIN);
+      case MEMBER -> true;
+    };
   }
 }
