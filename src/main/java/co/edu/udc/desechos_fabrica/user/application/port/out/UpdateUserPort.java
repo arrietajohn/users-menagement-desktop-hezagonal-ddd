7f170b746338d32a7da54350fe1ecc2910de5399
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-05-30 14:43:39 -0500

    feat(application): include currentEmail in UpdateUserPort

--- src/main/java/co/edu/udc/desechos_fabrica/user/application/port/out/UpdateUserPort.java
+++ src/main/java/co/edu/udc/desechos_fabrica/user/application/port/out/UpdateUserPort.java
@@ -1,7 +1,8 @@
 package co.edu.udc.desechos_fabrica.user.application.port.out;
 
 import co.edu.udc.desechos_fabrica.user.domain.model.UserModel;
+import co.edu.udc.desechos_fabrica.user.domain.valueobject.UserEmail;
 
 public interface UpdateUserPort {
-  UserModel update(UserModel user);
+  UserModel update(UserEmail currentEmail, UserModel user);
 }
