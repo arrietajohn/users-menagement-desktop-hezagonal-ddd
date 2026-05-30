aa9267e25210ce5de0b261406198a53c5b948cee
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-05-29 19:33:21 -0500

    refactor(user/application): replace user id with email in get user query

--- src/main/java/co/edu/udc/desechos_fabrica/user/application/service/dto/query/GetUserByEmailQuery.java
+++ src/main/java/co/edu/udc/desechos_fabrica/user/application/service/dto/query/GetUserByEmailQuery.java
@@ -2,7 +2,7 @@ package co.edu.udc.desechos_fabrica.user.application.service.dto.query;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record GetUserByIdQuery(@NotBlank(message = "id must not be blank") String id)
+public record GetUserByEmailQuery(@NotBlank(message = "email must not be blank") String email)
 {
 
 }
