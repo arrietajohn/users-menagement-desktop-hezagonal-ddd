1ba2f79416e40c3747277ea3b34a8cae2f509fe9
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-05-29 22:11:16 -0500

    refactor(infrastructure): add firstName and lastName to user persistence

--- src/main/java/co/edu/udc/desechos_fabrica/user/infrastructure/adapter/persistence/dto/UserPersistenceDto.java
+++ src/main/java/co/edu/udc/desechos_fabrica/user/infrastructure/adapter/persistence/dto/UserPersistenceDto.java
@@ -1,8 +1,8 @@
 package co.edu.udc.desechos_fabrica.user.infrastructure.adapter.persistence.dto;
 
 public record UserPersistenceDto(
-    String id,
-    String name,
+    String firstName,
+    String lastName,
     String email,
     String password,
     String role,
