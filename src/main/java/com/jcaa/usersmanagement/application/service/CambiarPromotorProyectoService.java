35b482014af40302eb93df12d58ecc07a3101336
Sebastian Arango <sarangos1@unicartagena.edu.co>
2026-05-30 11:06:36 -0500

    feat: add CambiarPromotorProyectoService application service

--- src/main/java/com/jcaa/usersmanagement/application/service/CambiarPromotorProyectoService.java
+++ src/main/java/com/jcaa/usersmanagement/application/service/CambiarPromotorProyectoService.java
@@ -1,4 +1,18 @@
 package com.jcaa.usersmanagement.application.service;
 
+import com.jcaa.usersmanagement.domain.ports.ProyectoRepository;
+
 public class CambiarPromotorProyectoService {
+    private final ProyectoRepository repository;
+
+    public CambiarPromotorProyectoService(ProyectoRepository repository) {
+        this.repository = repository;
+    }
+
+    public void execute(Long idProyecto, Long nuevoIdPromotor) {
+        if (idProyecto == null || nuevoIdPromotor == null) {
+            throw new IllegalArgumentException("El ID del proyecto y el ID del nuevo promotor son obligatorios.");
+        }
+        this.repository.updatePromotor(idProyecto, nuevoIdPromotor);
+    }
 }
