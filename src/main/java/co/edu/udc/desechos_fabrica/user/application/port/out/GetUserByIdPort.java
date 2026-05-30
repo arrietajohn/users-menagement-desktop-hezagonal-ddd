133a4456aeea9182a07201b921b96379dce10382
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-05-30 16:34:10 -0500

    refactor(user/domain): remove user id usage from domain logic

--- src/main/java/co/edu/udc/desechos_fabrica/user/application/port/out/GetUserByIdPort.java
+++
@@ -1,9 +0,0 @@
-package co.edu.udc.desechos_fabrica.user.application.port.out;
-
-import co.edu.udc.desechos_fabrica.user.domain.model.UserModel;
-import co.edu.udc.desechos_fabrica.user.domain.valueobject.UserId;
-import java.util.Optional;
-
-public interface GetUserByIdPort {
-  Optional<UserModel> getById(UserId userId);
-}
