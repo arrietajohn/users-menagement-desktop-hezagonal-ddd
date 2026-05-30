8449528aea673de5e3d4b58356f7772cf009658a
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-05-30 16:31:44 -0500

    refactor(application): update UserMapper to include firstName and lastName and replace id with email in methods

--- src/main/java/co/edu/udc/desechos_fabrica/user/application/service/mapper/UserApplicationMapper.java
+++ src/main/java/co/edu/udc/desechos_fabrica/user/application/service/mapper/UserApplicationMapper.java
@@ -3,14 +3,12 @@ package co.edu.udc.desechos_fabrica.user.application.service.mapper;
 import co.edu.udc.desechos_fabrica.user.application.service.dto.command.CreateUserCommand;
 import co.edu.udc.desechos_fabrica.user.application.service.dto.command.DeleteUserCommand;
 import co.edu.udc.desechos_fabrica.user.application.service.dto.command.UpdateUserCommand;
-import co.edu.udc.desechos_fabrica.user.application.service.dto.query.GetUserByIdQuery;
+import co.edu.udc.desechos_fabrica.user.application.service.dto.query.GetUserByEmailQuery;
 import co.edu.udc.desechos_fabrica.user.domain.enums.UserRole;
 import co.edu.udc.desechos_fabrica.user.domain.enums.UserStatus;
 import co.edu.udc.desechos_fabrica.user.domain.model.UserModel;
-import co.edu.udc.desechos_fabrica.user.domain.valueobject.UserEmail;
-import co.edu.udc.desechos_fabrica.user.domain.valueobject.UserId;
-import co.edu.udc.desechos_fabrica.user.domain.valueobject.UserFirstName;
-import co.edu.udc.desechos_fabrica.user.domain.valueobject.UserPassword;
+import co.edu.udc.desechos_fabrica.user.domain.valueobject.*;
+
 import java.util.Objects;
 import lombok.experimental.UtilityClass;
 
@@ -19,32 +17,40 @@ public class UserApplicationMapper {
 
   public UserModel fromCreateCommandToModel(final CreateUserCommand command) {
     return UserModel.create(
-            new UserFirstName(command.name()),
+        new UserFirstName(command.firstName()),
+        new UserLastName(command.lastName()),
         new UserEmail(command.email()),
         UserPassword.fromPlainText(command.password()),
         UserRole.fromString(command.role()));
   }
 
   public UserModel fromUpdateCommandToModel(
-      final UpdateUserCommand command, final UserPassword currentPassword) {
+      final UpdateUserCommand command, final UserModel currentUser) {
 
-    final UserPassword passwordToUse = resolvePassword(command.password(), currentPassword);
+    final UserPassword passwordToUse = resolvePassword(command.password(), currentUser.getPassword());
+    final UserRole roleToUse = (command.role() != null)
+            ? UserRole.fromString(command.role())
+            : currentUser.getRole();
+
+    final UserStatus statusToUse = (command.status() != null)
+            ? UserStatus.fromString(command.status())
+            : currentUser.getStatus();
 
     return new UserModel(
-        new UserId(command.id()),
-        new UserFirstName(command.name()),
+        new UserFirstName(command.firstName()),
+        new UserLastName(command.lastName()),
         new UserEmail(command.email()),
         passwordToUse,
-        UserRole.fromString(command.role()),
-        UserStatus.fromString(command.status()));
+        roleToUse,
+        statusToUse);
   }
 
-  public UserId fromGetUserByIdQueryToUserId(final GetUserByIdQuery query) {
-    return new UserId(query.id());
+  public UserEmail fromGetUserByEmailQueryToUserEmail(final GetUserByEmailQuery query) {
+    return new UserEmail(query.email());
   }
 
-  public UserId fromDeleteCommandToUserId(final DeleteUserCommand command) {
-    return new UserId(command.id());
+  public UserEmail fromDeleteCommandToUserEmail(final DeleteUserCommand command) {
+    return new UserEmail(command.email());
   }
 
   private UserPassword resolvePassword(
