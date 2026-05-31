e702a2505240f379f90e1efd8f073e3f9d5851ca
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-05-31 11:19:10 -0500

    feat(enterprise/domain): add InvalidEnterpriseNameException

---
+++ src/main/java/co/edu/udc/desechos_fabrica/enterprise/domain/exception/InvalidEnterpriseNameException.java
@@ -0,0 +1,21 @@
+package co.edu.udc.desechos_fabrica.enterprise.domain.exception;
+
+import co.edu.udc.desechos_fabrica.user.domain.exception.DomainException;
+
+public final class InvalidEnterpriseNameException extends DomainException {
+
+     private static final String MESSAGE_EMPTY = "The enterprise name must not be empty.";
+     private static final String MESSAGE_TOO_SHORT = "The user name must have at least %d characters.";
+
+     private InvalidEnterpriseNameException(String message) {
+        super(message);
+    }
+
+    public static InvalidEnterpriseNameException becauseValueIsEmpty() {
+        return new InvalidEnterpriseNameException(MESSAGE_EMPTY);
+    }
+
+    public static InvalidEnterpriseNameException becauseLengthIsTooShort(final int minimumLength) {
+        return new InvalidEnterpriseNameException(String.format(MESSAGE_TOO_SHORT, minimumLength));
+    }
+}
