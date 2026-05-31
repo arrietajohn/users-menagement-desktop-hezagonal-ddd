14e0197e8fcf1ff5bdf100944912b99162be28fd
José Luis Arteta Buelvas <jartetab@unicartagena.edu.co>
2026-05-31 01:14:36 -0500

    fix(application): corrigiendo errores en los puertos

--- src/main/java/com/jcaa/usersmanagement/application/port/in/UpdateSucursalUseCase.java
+++ src/main/java/com/jcaa/usersmanagement/application/port/in/UpdateSucursalUseCase.java
@@ -1,9 +1,10 @@
 package com.jcaa.usersmanagement.application.port.in;
-import com.jcaa.usersmanagement.application.service.dto.command.UpdateUserCommand;
+import com.jcaa.usersmanagement.application.service.dto.command.UpdateSucursalCommand;
+import com.jcaa.usersmanagement.domain.model.SucursalModel;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
 
 public interface UpdateSucursalUseCase {
-    void execute(@NotNull @Valid UpdateUserCommand command);
+    SucursalModel execute(@NotNull @Valid UpdateSucursalCommand command);
 }
--- src/main/java/com/jcaa/usersmanagement/application/port/out/UpdateSucursalPort.java
+++ src/main/java/com/jcaa/usersmanagement/application/port/out/UpdateSucursalPort.java
@@ -1,6 +1,7 @@
 package com.jcaa.usersmanagement.application.port.out;
-import com.jcaa.usersmanagement.domain.model.UserModel;
+import com.jcaa.usersmanagement.domain.model.SucursalModel;
+import com.jcaa.usersmanagement.domain.valueobject.SucursaId;
 
 public interface UpdateSucursalPort {
-    UserModel update(UserModel user);
+    SucursalModel update(SucursalModel sucursalToUpdate);
 }
