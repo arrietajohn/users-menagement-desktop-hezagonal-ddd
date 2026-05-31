28a20dcd9a48b451466bac71617b924df1d0eb87
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-05-31 11:04:26 -0500

    refactor(enterprise/domain):  add regex to centralize NIT format validation logic

--- src/main/java/co/edu/udc/desechos_fabrica/enterprise/domain/valueobject/EnterpriseNit.java
+++ src/main/java/co/edu/udc/desechos_fabrica/enterprise/domain/valueobject/EnterpriseNit.java
@@ -1,18 +1,17 @@
 package co.edu.udc.desechos_fabrica.enterprise.domain.valueobject;
 
 import java.util.Objects;
+import java.util.regex.Pattern;
 import co.edu.udc.desechos_fabrica.enterprise.domain.exception.InvalidEnterpriseNitException;
 
 public record EnterpriseNit (String value) {
 
-    private static final int MINIMUM_LENGTH = 9;
-    private static final int MAXIMUM_LENGTH = 12;
-
+    private static final Pattern ENTERPRISE_PATTERN = Pattern.compile("^[0-9]{9,12}$");
 
     public EnterpriseNit {
         final String normalizedValue = Objects.requireNonNull(value, "Enterprise Nit can not be null").trim();
         validateNotEmpty(normalizedValue);
-        validateLength(normalizedValue);
+        validateFormat(normalizedValue);
         value = normalizedValue;
     }
 
@@ -22,9 +21,9 @@ public record EnterpriseNit (String value) {
         }
     }
 
-    private static void validateLength(final String normalizedValue) {
-        if (normalizedValue.length() < MINIMUM_LENGTH || normalizedValue.length() > MAXIMUM_LENGTH) {
-            throw InvalidEnterpriseNitException.becauseLengthIsInvalid(MINIMUM_LENGTH, MAXIMUM_LENGTH);
+    private void validateFormat(final String normalizedValue){
+        if (!ENTERPRISE_PATTERN.matcher(normalizedValue).matches()) {
+            throw InvalidEnterpriseNitException.becauseFormatIsInvalid();
         }
     }
 }
