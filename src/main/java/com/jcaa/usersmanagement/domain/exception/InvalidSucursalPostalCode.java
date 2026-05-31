f0f5fad68f5f56d051797fef80848603966737b3
José Luis Arteta Buelvas <jartetab@unicartagena.edu.co>
2026-05-30 21:48:43 -0500

    feat(domain): agrega InvalidSucursalPostalCode como exception

---
+++ src/main/java/com/jcaa/usersmanagement/domain/exception/InvalidSucursalPostalCode.java
@@ -0,0 +1,12 @@
+package com.jcaa.usersmanagement.domain.exception;
+
+public class InvalidSucursalPostalCode extends RuntimeException {
+    private static final String MESSAGE_EMPTY = "El código postal no puede ir vacío";
+    public InvalidSucursalPostalCode(String message) {
+        super(message);
+    }
+
+    public static InvalidSucursalPostalCode becauseValueIsEmpty() {
+        return new InvalidSucursalPostalCode(MESSAGE_EMPTY);
+    }
+}
