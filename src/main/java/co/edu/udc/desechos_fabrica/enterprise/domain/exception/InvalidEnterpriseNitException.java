6e648ea50296930e9b7e123de0512ea5ed6cec49
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-05-31 01:18:49 -0500

    refactor(enterprise/domain): refactor static methods for enterprise nit validation

--- src/main/java/co/edu/udc/desechos_fabrica/enterprise/domain/exception/InvalidEnterpriseNitException.java
+++ src/main/java/co/edu/udc/desechos_fabrica/enterprise/domain/exception/InvalidEnterpriseNitException.java
@@ -5,7 +5,7 @@ import co.edu.udc.desechos_fabrica.user.domain.exception.DomainException;
 public final class InvalidEnterpriseNitException extends DomainException{
 
   private static final String MESSAGE_EMPTY = "The enterprise nit must not be empty.";
-  private static final String MESSAGE_TOO_SHORT = "The enterprise nit must have at least %d characters.";
+  private static final String MESSAGE_CHARACTER_LENGTH = "The enterprise nit must have at least %d and most %d characters.";
 
   private InvalidEnterpriseNitException(final String message) {
     super(message);
@@ -15,7 +15,8 @@ public final class InvalidEnterpriseNitException extends DomainException{
     return new InvalidEnterpriseNitException(MESSAGE_EMPTY);
   }
 
-  public static InvalidEnterpriseNitException becauseValueIsTooShort(final int minimumLength) {
-    return new InvalidEnterpriseNitException(String.format(MESSAGE_TOO_SHORT, minimumLength));
+  public static InvalidEnterpriseNitException becauseLengthIsInvalid(final int minimumLength, final int maximumLength) {
+    return new InvalidEnterpriseNitException(String.format(MESSAGE_CHARACTER_LENGTH, minimumLength, maximumLength));
   }
+
 }
