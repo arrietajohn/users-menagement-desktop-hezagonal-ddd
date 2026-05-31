357bda89a9d9fb3acd737f6c46690aa2103d6069
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-05-31 00:58:01 -0500

    feat(enterprise/domain): add InvalidEnterpriseNitException extending DomainException

---
+++ src/main/java/co/edu/udc/desechos_fabrica/enterprise/domain/exception/InvalidEnterpriseNitException.java
@@ -0,0 +1,13 @@
+package co.edu.udc.desechos_fabrica.enterprise.domain.exception;
+
+import co.edu.udc.desechos_fabrica.user.domain.exception.DomainException;
+
+public final class InvalidEnterpriseNitException extends DomainException{
+
+  private static final String MESSAGE_EMPTY = "The enterprise nit must not be empty.";
+  private static final String MESSAGE_TOO_SHORT = "The enterprise nit must have at least %d characters.";
+
+  private InvalidEnterpriseNitException(final String message) {
+    super(message);
+  }
+}
