2b0b8fd901dec42cf940fba064953f9856f3470d
José Luis Arteta Buelvas <jartetab@unicartagena.edu.co>
2026-05-30 20:10:43 -0500

    feat(domain): agrega SucursalNotFoundException como exception

--- src/main/java/com/jcaa/usersmanagement/domain/exception/SucursalNotFoundException.java
+++ src/main/java/com/jcaa/usersmanagement/domain/exception/SucursalNotFoundException.java
@@ -1,13 +1,13 @@
 package com.jcaa.usersmanagement.domain.exception;
 
-public final class SucursalNumberNotFoundException extends DomainException {
-    private static final String MESSAGE_BY_NUMBER = "El número de la sucursal no fue encontrado";
-    private SucursalNumberNotFoundException(String message) {
+public final class SucursalNotFoundException extends DomainException {
+    private static final String MESSAGE_BY_NUMBER = "La sucursal no fue encontrada";
+    private SucursalNotFoundException(String message) {
         super(message);
     }
 
-    public static SucursalNumberNotFoundException becauseNumberWasNotFound(final Integer number){
-        return new SucursalNumberNotFoundException(String.format(MESSAGE_BY_NUMBER, number));
+    public static SucursalNotFoundException becauseNumberWasNotFound(final Integer number){
+        return new SucursalNotFoundException(String.format(MESSAGE_BY_NUMBER, number));
 
     }
 }
