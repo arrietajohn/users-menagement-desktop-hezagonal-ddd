9d02515d9fc0b20a55f275b973768bf8140d9756
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-05-31 11:23:08 -0500

    feat(enterprise/domain): add EnterpriseName value object with validation

---
+++ src/main/java/co/edu/udc/desechos_fabrica/enterprise/domain/valueobject/EnterpriseName.java
@@ -0,0 +1,33 @@
+package co.edu.udc.desechos_fabrica.enterprise.domain.valueobject;
+
+import co.edu.udc.desechos_fabrica.enterprise.domain.exception.InvalidEnterpriseNameException;
+import java.util.Objects;
+
+public record EnterpriseName(String value) {
+
+    private static final int MINIMUM_LENGTH = 3;
+
+    public EnterpriseName {
+        final String normalizedValue = Objects.requireNonNull(value, "user first name cannot be null").trim();
+        validateNotEmpty(normalizedValue);
+        validateMinimumLength(normalizedValue);
+        value = normalizedValue;
+    }
+
+    private static void validateNotEmpty(final String normalizedValue) {
+        if (normalizedValue.isEmpty()) {
+            throw InvalidEnterpriseNameException.becauseValueIsEmpty();
+        }
+    }
+
+    private static void validateMinimumLength(final String normalizedValue) {
+        if (normalizedValue.length() < MINIMUM_LENGTH) {
+            throw InvalidEnterpriseNameException.becauseLengthIsTooShort(MINIMUM_LENGTH);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
