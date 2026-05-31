e7e82d188921c349221f39d59e694c6eb09addc4
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-05-31 12:23:57 -0500

    feat(enterprise/domain): add activation and deactivation logic

--- src/main/java/co/edu/udc/desechos_fabrica/enterprise/domain/model/EnterpriseModel.java
+++ src/main/java/co/edu/udc/desechos_fabrica/enterprise/domain/model/EnterpriseModel.java
@@ -3,6 +3,7 @@ package co.edu.udc.desechos_fabrica.enterprise.domain.model;
 import co.edu.udc.desechos_fabrica.enterprise.domain.valueobject.EnterpriseName;
 import co.edu.udc.desechos_fabrica.enterprise.domain.valueobject.EnterpriseNit;
 import co.edu.udc.desechos_fabrica.enterprise.domain.enums.EnterpriseStatus;
+import co.edu.udc.desechos_fabrica.user.domain.model.UserModel;
 import lombok.Value;
 
 @Value
@@ -17,5 +18,13 @@ public class EnterpriseModel {
         final EnterpriseName name) {
         return new EnterpriseModel(nit, name, EnterpriseStatus.PENDING);
     }
-    
+
+    public EnterpriseModel activate() {
+        return new EnterpriseModel(nit, name, EnterpriseStatus.ACTIVE);
+    }
+
+    public EnterpriseModel deactivate() {
+        return new EnterpriseModel(nit, name, EnterpriseStatus.INACTIVE);
+    }
+
 }
