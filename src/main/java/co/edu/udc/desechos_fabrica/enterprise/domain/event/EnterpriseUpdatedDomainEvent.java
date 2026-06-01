622fca8df8a0012b1c961d7fa633875160fbac9b
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-05-31 21:24:13 -0500

    feat(enterprise/domain): add EnterpriseUpdatedDomainEvent

---
+++ src/main/java/co/edu/udc/desechos_fabrica/enterprise/domain/event/EnterpriseUpdatedDomainEvent.java
@@ -0,0 +1,27 @@
+package co.edu.udc.desechos_fabrica.enterprise.domain.event;
+
+import co.edu.udc.desechos_fabrica.shared.DomainEvent;
+import co.edu.udc.desechos_fabrica.enterprise.domain.model.EnterpriseModel;
+import java.util.Map;
+import lombok.Getter;
+
+@Getter
+public class EnterpriseUpdatedDomainEvent extends DomainEvent {
+
+    private static final String EVENT_NAME = "enterprise.updated";
+
+    private final EnterpriseModel enterprise;
+
+    public EnterpriseUpdatedDomainEvent(final EnterpriseModel enterprise) {
+        super(EVENT_NAME);
+        this.enterprise = enterprise;
+    }
+
+    @Override
+    public Map<String, String> payload() {
+        return Map.of(
+                "nit", enterprise.getNit().value(),
+                "name", enterprise.getName().value(),
+                "status", enterprise.getStatus().name());
+    }
+}
