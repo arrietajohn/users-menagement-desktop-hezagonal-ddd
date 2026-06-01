493fff8e9c9f230b49396662cb1f52f98053774a
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-06-01 15:36:20 -0500

    test(enterprise/domain): add parameterized tests and null input validation
    ``

--- src/test/java/co/edu/udc/desechos_fabrica/enterprise/domain/valueobject/EnterpriseNameTest.java
+++ src/test/java/co/edu/udc/desechos_fabrica/enterprise/domain/valueobject/EnterpriseNameTest.java
@@ -3,6 +3,9 @@ package co.edu.udc.desechos_fabrica.enterprise.domain.valueobject;
 import co.edu.udc.desechos_fabrica.enterprise.domain.exception.InvalidEnterpriseNameException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class EnterpriseNameTest {
@@ -14,10 +17,18 @@ public class EnterpriseNameTest {
         assertDoesNotThrow(() -> new EnterpriseName(name));
     }
 
-    @Test
-    @DisplayName("Should throw InvalidEnterpriseNameException with invalid name")
-    public void testInvalidEnterpriseNameCreation(){
-        final String invalidName = "";
-        assertThrows(InvalidEnterpriseNameException.class, () -> new EnterpriseName(invalidName));
+    @ParameterizedTest
+    @ValueSource(
+            strings = {"", "  ", "\t", "\n", "\r", "\f", "\b", "EC", "RE  ", "", "   SA ", "OS\t"})
+    @DisplayName("should throw InvalidEnterpriseNameException for empty names or names shorter than 3 characters")
+    public void testShortEnterpriseNameCreation(final String enterpriseName){
+        assertThrows(InvalidEnterpriseNameException.class, () -> new EnterpriseName(enterpriseName));
     }
+
+    @Test
+    @DisplayName("Should throw NullPointerException with null name")
+    public void testNullEnterpriseNameCreation(){
+        assertThrows(NullPointerException.class, () -> new EnterpriseName(null));
+    }
+
 }
