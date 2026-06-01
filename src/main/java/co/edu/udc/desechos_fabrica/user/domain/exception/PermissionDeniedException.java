e45d7e39793ac88bffe2f68c1941217f8fa3e7f4
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-06-01 01:47:38 -0500

    feat(user/domain): add PermissionDeniedException

---
+++ src/main/java/co/edu/udc/desechos_fabrica/user/domain/exception/PermissionDeniedException.java
@@ -0,0 +1,21 @@
+package co.edu.udc.desechos_fabrica.user.domain.exception;
+
+import co.edu.udc.desechos_fabrica.shared.DomainException;
+
+public class PermissionDeniedException extends DomainException {
+
+    private static final String MESSAGE_PERMISSION_UPDATED = "Note: You cannot change the user role. It will remain unchanged.";
+    private static final String MESSAGE_PERMISSION_DELETED = "Note: You cannot delete the user. It will remain unchanged.";
+
+    public PermissionDeniedException(String message) {
+        super(message);
+    }
+
+    public static PermissionDeniedException becauseHaveNotUpdatePermission(final String email) {
+        return new PermissionDeniedException(MESSAGE_PERMISSION_UPDATED);
+    }
+
+    public static PermissionDeniedException becauseHaveNotDeletePermission(final String email) {
+        return new PermissionDeniedException(MESSAGE_PERMISSION_DELETED);
+    }
+}
