c2a62ffa581b564ca82a8720069918467510077b
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-06-02 17:46:34 -0500

    feat(location/domain): add validation rules to LocationName

--- src/main/java/co/edu/udc/desechos_fabrica/location/domain/valueobjects/LocationName.java
+++ src/main/java/co/edu/udc/desechos_fabrica/location/domain/valueobjects/LocationName.java
@@ -1,13 +1,30 @@
 package co.edu.udc.desechos_fabrica.location.domain.valueobjects;
 
+import co.edu.udc.desechos_fabrica.location.domain.exception.InvalidLocationNameException;
+
 import java.util.Objects;
 
 public record LocationName(String locationName) {
 
-    private static final int MINIMUN_LENGHT = 3;
+    private static final int MINIMUM_LENGTH = 3;
 
     public LocationName {
         final String normalizedValue = Objects.requireNonNull(locationName, "Location name can not be null").trim();
+        validateNotEmpty(normalizedValue);
+        validateMinimumLength(normalizedValue);
+        locationName = normalizedValue;
+    }
+
+    public void validateNotEmpty(final String normalizedValue){
+        if (normalizedValue.isEmpty()) {
+            throw InvalidLocationNameException.becauseValueIsEmpty();
+        }
+    }
+
+    public void validateMinimumLength(final String normalizedValue){
+        if (normalizedValue.length() < MINIMUM_LENGTH){
+            throw InvalidLocationNameException.becauseLengthIsTooShort(MINIMUM_LENGTH);
+        }
     }
 
     @Override
