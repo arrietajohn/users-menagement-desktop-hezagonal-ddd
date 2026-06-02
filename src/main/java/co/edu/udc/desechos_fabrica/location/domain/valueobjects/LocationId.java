0fe4fffbac9defc6ff2a0ca13cbb8d438e20ad6e
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-06-02 00:46:28 -0500

    feat(location/domain): add empty value validation for LocationId

--- src/main/java/co/edu/udc/desechos_fabrica/location/domain/valueobjects/LocationId.java
+++ src/main/java/co/edu/udc/desechos_fabrica/location/domain/valueobjects/LocationId.java
@@ -1,11 +1,21 @@
 package co.edu.udc.desechos_fabrica.location.domain.valueobjects;
 
 import java.util.Objects;
+import co.edu.udc.desechos_fabrica.location.domain.exception.InvalidLocationIdException;
 
-public record LocationId (String value){
+
+public record LocationId(String value){
 
     public LocationId{
         final String normalizedValue = Objects.requireNonNull(value, "Location Id can not be null").trim();
+        validateNotEmpty(normalizedValue);
+        value = normalizedValue;
+    }
+
+    private static void validateNotEmpty(final String normalizedValue) {
+        if (normalizedValue.isEmpty()) {
+            throw InvalidLocationIdException.becauseValueIsEmpty();
+        }
     }
 
     @Override
