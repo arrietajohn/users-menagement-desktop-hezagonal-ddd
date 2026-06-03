1c28a9cc313ea7c0deb85c2e143408f3e1f49787
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-06-02 22:47:16 -0500

    feat(location/domain): add behavior to update attributes and manage Location state

--- src/main/java/co/edu/udc/desechos_fabrica/location/domain/model/LocationModel.java
+++ src/main/java/co/edu/udc/desechos_fabrica/location/domain/model/LocationModel.java
@@ -4,17 +4,18 @@ import co.edu.udc.desechos_fabrica.enterprise.domain.valueobject.EnterpriseId;
 import co.edu.udc.desechos_fabrica.location.domain.valueobjects.*;
 import co.edu.udc.desechos_fabrica.location.domain.enums.*;
 import lombok.Value;
+import lombok.NonNull;
 
 @Value
 public class LocationModel {
 
     LocationId id;
-    LocationName name;
-    LocationCity city;
-    EnterpriseId enterpriseId;
-    LocationState state;
-    LocationCountry country;
-    LocationCoordinate coordinate;
+    @NonNull LocationName name;
+    @NonNull LocationCity city;
+    @NonNull EnterpriseId enterpriseId;
+    @NonNull LocationState state;
+    @NonNull LocationCountry country;
+    @NonNull LocationCoordinate coordinate;
     LocationStatus status;
 
     public static LocationModel create(
@@ -26,6 +27,23 @@ public class LocationModel {
             final LocationCountry country,
             final LocationCoordinate coordinate) {
         return new LocationModel(id, name, city, enterpriseId, state, country, coordinate, LocationStatus.ACTIVE);
+    }
+
+    public LocationModel updateWith(
+            final LocationName newName,
+            final LocationCity newCity,
+            final LocationState newState,
+            final LocationCountry newCountry,
+            final LocationCoordinate newCoordinate) {
+        return new LocationModel(id, newName, newCity, enterpriseId, newState, newCountry, newCoordinate, status);
+    }
+
+    public LocationModel activate() {
+        return new LocationModel(id, name, city, enterpriseId, state, country, coordinate, LocationStatus.ACTIVE);
+    }
+
+    public LocationModel deactivate() {
+        return new LocationModel(id, name, city, enterpriseId, state, country, coordinate, LocationStatus.INACTIVE);
     }
 
 }
