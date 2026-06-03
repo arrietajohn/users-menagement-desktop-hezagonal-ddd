f3bbb0b95fe5f4840f8dfeac3f0f02e5074c62fe
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-06-02 20:56:12 -0500

    feat(location/domain): add LocationState with empty and length validation

---
+++ src/main/java/co/edu/udc/desechos_fabrica/location/domain/valueobjects/LocationState.java
@@ -0,0 +1,34 @@
+package co.edu.udc.desechos_fabrica.location.domain.valueobjects;
+
+import co.edu.udc.desechos_fabrica.location.domain.exception.InvalidLocationStateException;
+
+import java.util.Objects;
+
+public record LocationState(String state) {
+
+    private static final int MINIMUM_LENGTH = 3;
+
+    public LocationState {
+        final String normalizedValue = Objects.requireNonNull(state, "Location city can not be null").trim();
+        validateNotEmpty(normalizedValue);
+        validateMinimumLength(normalizedValue);
+        state = normalizedValue;
+    }
+
+    public void validateNotEmpty(final String normalizedValue) {
+        if (normalizedValue.isEmpty()) {
+            throw InvalidLocationStateException.becauseValueIsEmpty();
+        }
+    }
+
+    public void validateMinimumLength(final String normalizedValue) {
+        if (normalizedValue.length() < MINIMUM_LENGTH) {
+            throw InvalidLocationStateException.becauseLengthIsTooShort(MINIMUM_LENGTH);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return state;
+    }
+}
