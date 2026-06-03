d5101bf018b822252e89022eefc159932ec36061
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-06-02 20:37:39 -0500

    feat(enterprise/domain): add empty and format validation to EnterpriseId

--- src/main/java/co/edu/udc/desechos_fabrica/enterprise/domain/valueobject/EnterpriseId.java
+++ src/main/java/co/edu/udc/desechos_fabrica/enterprise/domain/valueobject/EnterpriseId.java
@@ -1,12 +1,28 @@
 package co.edu.udc.desechos_fabrica.enterprise.domain.valueobject;
 
+import co.edu.udc.desechos_fabrica.enterprise.domain.exception.InvalidEnterpriseIdException;
+
 import java.util.Objects;
 
 public record EnterpriseId(String value) {
 
     public EnterpriseId {
         final String enterpriseId = Objects.requireNonNull(value, "Enterprise Id can not be null").trim();
+        validateNotEmpty(enterpriseId);
+        validateFormat(enterpriseId);
         value = enterpriseId;
+    }
+
+    public void validateNotEmpty(final String normalizedValue) {
+        if (normalizedValue.isEmpty()) {
+            throw InvalidEnterpriseIdException.becauseValueIsEmpty();
+        }
+    }
+
+    public void validateFormat(final String normalizedValue) {
+        if (!normalizedValue.matches("^[0-9]+$")) {
+            throw InvalidEnterpriseIdException.becauseInvalidFormat();
+        }
     }
 
     @Override
