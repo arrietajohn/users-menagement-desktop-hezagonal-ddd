4a8ceb714708225dbbfcb38e070d1c7e1fae0657
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-06-02 20:54:45 -0500

    feat(location/domain): add InvalidLocationCityException

---
+++ src/main/java/co/edu/udc/desechos_fabrica/location/domain/exception/InvalidLocationCityException.java
@@ -0,0 +1,21 @@
+package co.edu.udc.desechos_fabrica.location.domain.exception;
+
+import co.edu.udc.desechos_fabrica.shared.DomainException;
+
+public class InvalidLocationCityException extends DomainException {
+
+    private static final String MESSAGE_EMPTY = "The location city must not be empty.";
+    private static final String MESSAGE_TOO_SHORT = "The location city must be at least %d characters long.";
+
+    public InvalidLocationCityException(String message) {
+        super(message);
+    }
+
+    public static InvalidLocationCityException becauseValueIsEmpty() {
+        return new InvalidLocationCityException(MESSAGE_EMPTY);
+    }
+
+    public static InvalidLocationCityException becauseLengthIsTooShort(int minimumLength) {
+        return new InvalidLocationCityException(String.format(MESSAGE_TOO_SHORT, minimumLength));
+    }
+}
