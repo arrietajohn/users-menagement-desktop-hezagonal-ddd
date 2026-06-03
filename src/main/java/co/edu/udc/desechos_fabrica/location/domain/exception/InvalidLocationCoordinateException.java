16aed1493b7bf9adbcb87e9a3ee681215a66a4c1
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-06-02 21:59:01 -0500

    feat(location/domain): add InvalidLocationCoordinateException

---
+++ src/main/java/co/edu/udc/desechos_fabrica/location/domain/exception/InvalidLocationCoordinateException.java
@@ -0,0 +1,22 @@
+package co.edu.udc.desechos_fabrica.location.domain.exception;
+
+import co.edu.udc.desechos_fabrica.shared.DomainException;
+
+public class InvalidLocationCoordinateException extends DomainException{
+
+    private static final String MESSAGE_EMPTY = "The location coordinate must not be empty.";
+    private static final String MESSAGE_INVALID_FORMAT = "Latitude must be numbers between -90 and 90, longitude must be numbers between -180 and 180.";
+
+
+    private InvalidLocationCoordinateException(final String message) {
+        super(message);
+    }
+
+    public static InvalidLocationCoordinateException becauseValueIsEmpty() {
+        return new InvalidLocationCoordinateException(MESSAGE_EMPTY);
+    }
+
+    public static InvalidLocationCoordinateException becauseInvalidFormat() {
+        return new InvalidLocationCoordinateException(MESSAGE_INVALID_FORMAT);
+    }
+}
